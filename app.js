885991c1944388d1b5721758c8209fb225d04191
SavageForum <savagejcd@googlemail.com>
2026-08-15 18:37:15 +0100

    Refactor magazine card HTML generation

--- app.js
+++ app.js
@@ -26,30 +26,29 @@ function displayMagazines(items) {
 
     items.forEach(magazine => {
 
-    container.innerHTML += 
-        <div class="mag-card">
+    container.innerHTML += `
+    <div class="mag-card">
 
-            .image}"
-                alt="${magazine.title}"
-                class="mag-image"
-            >
+        ${magazine.image}            alt="${magazine.title}"
+            class="mag-image"
+        >
 
-            <h3>${magazine.title}</h3>
+        <h3>${magazine.title}</h3>
 
-            <p>
-                ${magazine.month} ${magazine.year}
-            </p>
+        <p>
+            ${magazine.month} ${magazine.year}
+        </p>
 
-            <p>
-                Condition: ${magazine.condition}
-            </p>
+        <p>
+            Condition: ${magazine.condition}
+        </p>
 
-            <p>
-                £${magazine.price.toFixed(2)}
-            </p>
+        <p>
+            £${magazine.price.toFixed(2)}
+        </p>
 
-        </div>
-    ;
+    </div>
+`;
     });
 
 }
