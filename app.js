e9280d76e58886dcccabace36d35789e06391fd6
SavageForum <savagejcd@googlemail.com>
2026-08-15 18:06:00 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,77 @@
 console.log("APP.JS IS LOADED");
 
+let magazines = [];
+
 fetch("magazines.json")
     .then(response => response.json())
     .then(data => {
+
         console.log("JSON LOADED");
         console.log(data);
-    })
-    .catch(error => {
-        console.error(error);
+
+        magazines = data;
+
+        displayMagazines(magazines);
+    });
+
+function displayMagazines(items) {
+
+    const container = document.getElementById("magazine-list");
+
+    const count = document.getElementById("resultsCount");
+
+    count.textContent = `${items.length} magazine(s) found`;
+
+    container.innerHTML = "";
+
+    items.forEach(magazine => {
+
+        container.innerHTML += `
+            <div class="mag-card">
+                <h3>${magazine.title}</h3>
+
+                <p>
+                    ${magazine.month} ${magazine.year}
+                </p>
+
+                <p>
+                    Condition: ${magazine.condition}
+                </p>
+
+                <p>
+                    £${magazine.price.toFixed(2)}
+                </p>
+            </div>
+        `;
+    });
+
+}
+
+document
+    .getElementById("yearFilter")
+    .addEventListener("input", function () {
+
+        const year = parseInt(this.value);
+
+        if (isNaN(year)) {
+            displayMagazines(magazines);
+            return;
+        }
+
+        const filtered = magazines.filter(
+            magazine => magazine.year === year
+        );
+
+        displayMagazines(filtered);
+
+    });
+
+document
+    .getElementById("clearFilter")
+    .addEventListener("click", function () {
+
+        document.getElementById("yearFilter").value = "";
+
+        displayMagazines(magazines);
+
     });
