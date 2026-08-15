18e3ddd71c0442e84cc4a0ca24812f61b86b8001
SavageForum <savagejcd@googlemail.com>
2026-08-15 18:03:26 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log("APP.JS IS LOADED");
+
+fetch("magazines.json")
+    .then(response => response.json())
+    .then(data => {
+        console.log("JSON LOADED");
+        console.log(data);
+    })
+    .catch(error => {
+        console.error(error);
+    });
